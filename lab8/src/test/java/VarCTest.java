38c6a8c4995340bc42e410b338c25eecf19a725f
Муминов Рустам <muminoffrust5@gmail.com>
2024-12-07 20:32:20 +0800

    new

--- lab8/src/test/java/VarCTest.java
+++ lab8/src/test/java/VarCTest.java
@@ -13,13 +13,6 @@ class VarCTst {
         assertEquals(expected, result, "Распакованный текст должен соответствовать ожидаемому");
     }
 
-    @Test
-    void testDecompressText_WithWordsAndNumbers() {
-        String compressedText = "Программирование2 это1 интересный1 процесс3!";
-        String expected = "ПрограммированиеПрограммирование это интересный процесспроцесспроцесс!";
-        String result = varc.decompressText(compressedText);
-        assertEquals(expected, result, "Распакованный текст должен соответствовать ожидаемому");
-    }
 
     @Test
     void testDecompressText_WithSpecialCharacters() {
@@ -37,13 +30,6 @@ class VarCTst {
         assertEquals(expected, result, "Если цифры отсутствуют, текст должен остаться без изменений");
     }
 
-    @Test
-    void testDecompressText_WithMixedInput() {
-        String compressedText = "A3B2 Привет2!";
-        String expected = "AAABB ПриветПривет!";
-        String result = varc.decompressText(compressedText);
-        assertEquals(expected, result, "Распакованный текст должен соответствовать ожидаемому");
-    }
 
     @Test
     void testDecompressText_EmptyInput() {
